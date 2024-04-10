75ae6bfc1a30177f5f65e24bad8514bb3dcad74a
Pankaj Ghunawat <128151576+pankajswm@users.noreply.github.com>
2024-04-10 10:40:01 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,26 @@
+function myFunction() {
+   
+    copyText="0xb52f...47d";
+    navigator.clipboard.writeText(copyText);
+  // Alert the copied text
+  alert("Copied the text: " + copyText);
+  }
+
+
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+        document.getElementById("nav").style.backgroundColor = "#081526";
+        document.getElementById("nav").style.margin="0px"; 
+        document.getElementById("nav").style.padding="10px"; 
+        
+      } else {
+          document.getElementById("nav").style.backgroundColor = "transparent";
+          document.getElementById("nav").style.margin="15px"; 
+          document.getElementById("nav").style.padding="0px"; 
+
+
+      }
+}
